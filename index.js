5aaf323f7ec7372f7c16493356e59caff87f0e85
ConstantineGite <apostull@rambler.ru>
2018-04-13 16:55:12 +0300

    add style

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname));
+
 app.set('view engine', 'pug');
 
 app.get('/', (requesе, response)=>{
     response.render('index')
 })
 
-
 app.listen(3000, ()=>{
-
 	console.log("listen on http://localhost:3000");
-
 })
