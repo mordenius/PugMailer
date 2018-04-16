976cc4179f4649cceefd733f67b4c7858a4726e9
ConstantineGite <apostull@rambler.ru>
2018-04-16 18:02:50 +0300

    add value in json

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
-
+const pug = require("pug");
+const path = require("path");
+const dataJson = require("./data/textLayout.json");
+console.log(dataJson['EXP_1']['colum_1_1']['heder'])
 const app = express();
 
 app.use(express.static(__dirname));
 
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 
-app.get('/', (requesе, response)=>{
-    response.render('index')
+app.get('/', (req, res)=>{
+    // response.render('index')
+    const html = pug.renderFile(path.resolve("./views/index.pug"), { val: dataJson['EXP_1'] })
+    res.send(html)
 })
+
 
 app.get('/ar', (requesе, response)=>{
     response.render('ARIndex.pug')
 })
+
 
 app.listen(3000, ()=>{
 	console.log("listen on http://localhost:3000");
