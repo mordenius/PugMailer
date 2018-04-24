662cee7b527f6211d069068f84a4e79c896bedde
ConstantineGite <apostull@rambler.ru>
2018-04-24 18:23:36 +0300

    refactoring ||

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pug = require("pug");
 const path = require("path");
 const dataJson = require("./data/textLayout.json");
+const imageEightblock = require("./data/newData/infoAllTemplates.json")['middleBlockImage'];
 //console.log(dataJson['EXP_1']['colum_1_1']['heder'])
 const app = express();
 
@@ -10,12 +11,12 @@ app.use(express.static(__dirname));
 // app.set('view engine', 'pug');
 
 app.get('/', (req, res)=>{
-    const html = pug.renderFile(path.resolve("./views/index.pug"), { val: dataJson['exp1'], Data: dataJson['additionalInfo'] })
+    const html = pug.renderFile(path.resolve("./views/index.pug"), { val: dataJson['exp1'], Data: dataJson['additionalInfo'],     newData : imageEightblock['exp1'] })
     res.send(html)
 })
 
 app.get('/ar', (req, res)=>{
-	const html = pug.renderFile(path.resolve("./views/ARIndex.pug"), { val: dataJson['exp1'], Data: dataJson['additionalInfo'] })
+	const html = pug.renderFile(path.resolve("./views/ARIndex.pug"), { val: dataJson['exp1'], Data: dataJson['additionalInfo'],   newData: imageEightblock['exp1']  })
     res.send(html)
 })
 
