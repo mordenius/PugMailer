81bc0d8073fce629a52ed43a4f6f2e1a7914b8c1
ConstantineGite <apostull@rambler.ru>
2018-04-24 15:54:04 +0300

    chore(index.js) corected cey

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pug = require("pug");
 const path = require("path");
 const dataJson = require("./data/textLayout.json");
-console.log(dataJson['EXP_1']['colum_1_1']['heder'])
+//console.log(dataJson['EXP_1']['colum_1_1']['heder'])
 const app = express();
 
 app.use(express.static(__dirname));
@@ -10,12 +10,12 @@ app.use(express.static(__dirname));
 // app.set('view engine', 'pug');
 
 app.get('/', (req, res)=>{
-    const html = pug.renderFile(path.resolve("./views/index.pug"), { val: dataJson['PACK_1'], Data: dataJson['additionalInfo'] })
+    const html = pug.renderFile(path.resolve("./views/index.pug"), { val: dataJson['exp1'], Data: dataJson['additionalInfo'] })
     res.send(html)
 })
 
 app.get('/ar', (req, res)=>{
-	const html = pug.renderFile(path.resolve("./views/ARIndex.pug"), { val: dataJson['EXP_1'], Data: dataJson['additionalInfo'] })
+	const html = pug.renderFile(path.resolve("./views/ARIndex.pug"), { val: dataJson['exp1'], Data: dataJson['additionalInfo'] })
     res.send(html)
 })
 
