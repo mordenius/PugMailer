7474ae83609f56251466322bccaf01e23727b77d
apostullll1111 <constantineorisheach@gmail.com>
2018-04-25 08:03:36 +0300

    corectet style

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const pug = require("pug");
 const path = require("path");
 const dataJson = require("./data/textLayout.json");
 const imageEightblock = require("./data/newData/infoAllTemplates.json")['middleBlockImage'];
+const pcgExpText = require("./data/newData/midle_block/middle_block_en.json");
+//console.log(pcgExpText['exp1'], 'pcgExpText');
+
+
 //console.log(dataJson['EXP_1']['colum_1_1']['heder'])
 const app = express();
 
@@ -11,12 +15,20 @@ app.use(express.static(__dirname));
 // app.set('view engine', 'pug');
 
 app.get('/', (req, res)=>{
-    const html = pug.renderFile(path.resolve("./views/index.pug"), { val: dataJson['exp1'], Data: dataJson['additionalInfo'],     newData : imageEightblock['exp1'] })
+    const html = pug.renderFile(path.resolve("./views/index.pug"), { 
+    	val: dataJson['exp1'], Data: dataJson['additionalInfo'],
+    	newData : imageEightblock['exp1'],
+    	pcgExpText :  pcgExpText['exp1']
+    })
     res.send(html)
 })
 
 app.get('/ar', (req, res)=>{
-	const html = pug.renderFile(path.resolve("./views/ARIndex.pug"), { val: dataJson['exp1'], Data: dataJson['additionalInfo'],   newData: imageEightblock['exp1']  })
+	const html = pug.renderFile(path.resolve("./views/ARIndex.pug"),
+	{ val: dataJson['exp1'], Data: dataJson['additionalInfo'],   
+	newData: imageEightblock['exp1'],
+	pcgExpText :  pcgExpText['exp1']  
+	})
     res.send(html)
 })
 
